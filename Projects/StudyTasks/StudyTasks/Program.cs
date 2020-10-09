b740510e068ed54fd0a8b3ac45bf42f79057a07b
Cignid3001 <71765164+Cignid3001@users.noreply.github.com>
2020-10-09 17:11:16 +0300

    Test commit

--- Projects/StudyTasks/StudyTasks/Program.cs
+++ Projects/StudyTasks/StudyTasks/Program.cs
@@ -7,6 +7,7 @@ namespace StudyTasks
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
         }
     }
 }
