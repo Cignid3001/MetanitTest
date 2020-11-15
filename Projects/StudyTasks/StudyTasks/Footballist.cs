e59e7436dce28721286502c09c48e14a6d653af3
Cignid3001 <gnidos1488@yandex.ru>
2020-11-15 16:29:19 +0300

    new

---
+++ Projects/StudyTasks/StudyTasks/Footballist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyTasks
+{
+    class Footballist
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    class Team
+    {
+        Footballist[] footballists = new Footballist[11];
+
+        public Footballist this[int index]
+        {
+            get { return footballists[index]; }
+            set { footballists[index] = value; }
+        }
+    }
+}
---
+++ Projects/StudyTasks/StudyTasks/Gamer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyTasks
+{
+    class Gamer
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+    class Command
+    {
+        Gamer[] gamers = new Gamer[11];
+
+        public Gamer this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= gamers.Length)
+                    return gamers[index];
+                else
+                    return null;
+            }
+            set
+            {
+                if (index >= 0 && index < gamers.Length)
+                    gamers[index] = value;
+            }
+        }
+    }
+}
